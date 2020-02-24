4c21a7ea97cb43e55fff9373dd9ee14dbfeb487e
Daniel Buckmaster <daniel@crabmusket.net>
2020-02-25 09:20:33 +1100

    Test sum without AS loader

--- test_sum.ts
+++ test_sum.ts
@@ -1,17 +1,13 @@
-import * as loader from "./assemblyscript_loader.js";
-
 const memory = new WebAssembly.Memory({ initial: 1 });
 const floats = new Float64Array(memory.buffer);
 for (let i = 0; i < 10; i += 1) {
   floats[i] = 1;
 }
 
-const wasmModule = loader.instantiateSync(
-  Deno.readFileSync("./assembly/sum.runtime.wasm"),
-  // Pass the memory into the module in the "env" namespace, which is how
-  // AssemblyScript expects it when using the --importMemory flag.
+const lib = await WebAssembly.instantiate(
+  new Uint8Array(Deno.readFileSync("./assembly/sum.standalone.wasm")),
   { env: { memory } }
 );
 
-console.log("sum(0, 10) from module with runtime:");
-console.log(wasmModule.sum(0, 10));
+console.log("sum(0, 10) from standalone module:");
+console.log(lib.instance.exports.sum(0, 10));
